5e2ac5acaced770a33425f0cfcf8af125334f200
Your Name <najat_sf@hotmail.com>
2024-10-15 15:12:40 +0200

    connect to api

---
+++ backend/routes/cartRoute.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { addToCart,RemoveFromCart,getCart } from "../controllers/cartController.js"
+import authMiddleware from "../middleware/auth.js";
+
+const cartRouter = express.Router();
+
+cartRouter.post("/add",authMiddleware,addToCart)
+cartRouter.post("/remove",authMiddleware,RemoveFromCart)
+cartRouter.post("/get",authMiddleware,getCart)
+
+export default cartRouter;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+import 'dotenv/config'
 
 
 
@@ -19,6 +23,9 @@ connectDB();
 // api endpoint 
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 
 
 
