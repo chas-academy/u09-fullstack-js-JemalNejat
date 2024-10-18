119db504cc327eabc090ee47118d05851d52f792
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-18 11:44:36 +0200

    Update List.jsx

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import './List.css'
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { assets as frontendAssets } from '../../assets/frontend_assets/assets'
 
 const List = ({url}) => {
   
