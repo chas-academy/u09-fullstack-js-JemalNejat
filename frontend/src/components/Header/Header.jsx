30981d05a26920381ed613696d9cc69c05301b82
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-20 16:53:24 +0200

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header' style={{ backgroundImage: `url(${assets.header_2})` }} >
         <div className='header-contents'>
             <h2>Crave, Click, Enjoy!</h2>
             <p>Delicious meals are just a tap away! Explore a variety of cuisines and have your favorite dishes delivered to your doorstep. Quick, easy, and satisfying—order now!</p>
