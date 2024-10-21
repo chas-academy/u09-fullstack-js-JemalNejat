297a3bf1d7f1dae794378e3ab4cdebaf4d493094
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-21 15:31:42 +0200

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,14 @@
 import express from "express"
-import { loginUser,registerUser } from "../controllers/userController.js"
+import { loginUser,registerUser,loginAdmin } from "../controllers/userController.js"
 
 const userRouter = express.Router()
 
 userRouter.post("/register",registerUser)
 userRouter.post("/login",loginUser)
+userRouter.post("/login/admin", loginAdmin); 
 
 export default userRouter;
+
+
+
+
