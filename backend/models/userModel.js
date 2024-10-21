ca7b4b584b75e09bdf5c51dc7fc82b8f5ca6d31f
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-21 08:22:16 +0200

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    //role: {type:String,default:user},
+    role: {type:String,default:user},
     password: { type: String, required: true },
     cartData: { type: Object, default: {} },
 }, { minimize: false });
