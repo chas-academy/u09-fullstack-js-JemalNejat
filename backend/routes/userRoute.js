ae27b8b8ff03216e3673616b577e47f91357cc7d
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-22 18:53:48 +0200

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,17 +7,11 @@ const userRouter = express.Router()
 userRouter.post("/register",registerUser)
 userRouter.post("/login",loginUser)
 userRouter.post("/login/admin", loginAdmin); 
- // Fetch all users
+ 
   
    userRouter.get('/list', fetchAllUsers);
-
-   // Add user
    userRouter.post('/add', addUser);
-
-   // Update user
    userRouter.put('/update/:id', updateUser);
-
-   // Delete user
    userRouter.delete('/delete', deleteUser);
 
 export default userRouter;
