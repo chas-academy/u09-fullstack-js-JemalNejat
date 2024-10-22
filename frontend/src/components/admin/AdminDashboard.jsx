68e345188ebc2fcae3b25036e566f2643fb6d71c
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-22 22:15:34 +0200

    Update AdminDashboard.jsx

--- frontend/src/components/admin/AdminDashboard.jsx
+++ frontend/src/components/admin/AdminDashboard.jsx
@@ -1,15 +1,49 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import './AdminDashboard.css'
-import { assets } from '../../assets/frontend_assets/assets'
+import React from 'react';
+import './Navbar.css'; // Import Navbar CSS
+import './Sidebar.css'; // Import Sidebar CSS
+import { assets } from '../../assets/assets';
+import { NavLink } from 'react-router-dom';
 
-const AdminDashboard = () => {
+// Navbar Component
+const Navbar = () => {
   return (
-    <div>
-        <h1>Admin Dashboard</h1>
-        <p>Under construction test.</p>
+    <div className='navbar'>
+      <img className='logo' src={assets.mylogo} alt="Logo" />
+      <h2>Admin Panel</h2>
+      <img className='profile' src={assets.nejat} alt="Profile" />
     </div>
-  )
-}
+  );
+};
 
-export default AdminDashboard
+// Sidebar Component
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className='sidebar-options'>
+        <NavLink to='/add' className='sidebar-option'>
+          <img src={assets.add_icon} alt="Add Item" />
+          <p>Add Item</p>
+        </NavLink>
+
+        <NavLink to='/list' className='sidebar-option'>
+          <img src={assets.order_icon} alt="List Items" />
+          <p>List Items</p>
+        </NavLink>
+
+        <NavLink to='/orders' className='sidebar-option'>
+          <img src={assets.order_icon} alt="Orders" />
+          <p>Orders</p>
+        </NavLink>
+
+        <NavLink to='/users' className='sidebar-option'>
+          <img src={assets.profile_icon} alt="Users" />
+          <p>Users</p>
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+// Export both components
+export { Navbar, Sidebar };
