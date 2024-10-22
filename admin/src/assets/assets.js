6214b6695b1a8c1699d3a2ea3814aa72835896ea
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-22 13:26:57 +0200

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,6 +4,7 @@ import order_icon from './order_icon.png'
 
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
+import profile_icon from './profile_icon.png'
 import mylogo from './mylogo.webp'
 import nejat from './nejat.jpg'
 
@@ -17,7 +18,7 @@ export const assets ={
     logo,
     add_icon,
     order_icon,
-    
+    profile_icon,
     upload_area,
     parcel_icon,
     mylogo,
