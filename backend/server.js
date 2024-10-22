d38a233a7e7367c8734d0e1f2fb9cc26b4d06313
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-22 12:56:54 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
+import userRouter from './routes/userRoute.js';
  
 
 import 'dotenv/config'
@@ -28,6 +29,10 @@ app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
 app.use("/api/order",orderRouter)
 app.use("/api/admin",adminRouter)
+
+
+app.use("/api/user", userRouter);
+
 
 app.get("/",(req,res)=>{
     res.send("API Working")
