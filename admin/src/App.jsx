5cbbc91d54c7ec1bb4bdbc74d74ac62dbf10faea
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-22 12:17:24 +0200

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,7 +7,7 @@ import { Routes,Route } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
-import users from './pages/Users/users';
+import Users from './pages/Users/users';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -24,7 +24,7 @@ const App = () => {
           <Route path="/add" element={<Add url={url}/>}/>
           <Route path="/list" element={<List url={url}/>}/>
           <Route path="/orders" element={<Orders url={url}/>}/>
-          <Route path="/users" element={<users url={url}/>}/>
+          <Route path="/users" element={<Users url={url}/>}/>
         </Routes>
       </div>
       
