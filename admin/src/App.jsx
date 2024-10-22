6ae9db4b8318588d605bf0949e19cc3d4b385335
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-22 11:21:50 +0200

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,7 +7,7 @@ import { Routes,Route } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
-import Orders from './pages/Users/Users'
+import Users from './pages/Users/Users'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
