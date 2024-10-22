b078842f5b86169cc4bfa68a18902d21520b9742
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-22 12:36:51 +0200

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,14 +1,31 @@
 import express from "express"
-import { loginUser,registerUser,loginAdmin } from "../controllers/userController.js"
+import { loginUser,registerUser,loginAdmin } from "../controllers/userController.js";
+ import { addUser, updateUser, deleteUser, fetchAllUsers } from "../controllers/userController.js";
 
 const userRouter = express.Router()
 
 userRouter.post("/register",registerUser)
 userRouter.post("/login",loginUser)
 userRouter.post("/login/admin", loginAdmin); 
+ // Fetch all users
+   router.get('/list', fetchAllUsers); // Add this line to fetch users
+
+   // Add user
+   router.post('/add', addUser);
+
+   // Update user
+   router.put('/update/:id', updateUser);
+
+   // Delete user
+   router.delete('/delete', deleteUser);
 
 export default userRouter;
 
 
+ 
+ 
+  
+
+  
 
 
