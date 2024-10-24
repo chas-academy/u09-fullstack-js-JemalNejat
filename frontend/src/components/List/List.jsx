b783e092be8a78de612a247ca1826e057ba99db2
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-24 14:06:23 +0200

    Update List.jsx

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -1,1 +1,12 @@
-<p>test</p>
+// List.jsx
+import React from 'react';
+
+const List = () => {
+  return (
+    <div>
+      <p>test</p>
+    </div>
+  );
+};
+
+export default List;  // Ensure there is a default export
