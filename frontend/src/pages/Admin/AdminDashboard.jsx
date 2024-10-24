5f8620f105714dc7f04e344314570c45c8599ebd
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-24 12:08:06 +0200

    Update AdminDashboard.jsx

--- frontend/src/pages/Admin/AdminDashboard.jsx
+++ frontend/src/pages/Admin/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import './AdminDashboard.css';
+/* import './AdminDashboard.css';
 import Users from '../../components/Users/Users.jsx';
 
 const AdminDashboard = () => {
@@ -15,4 +15,64 @@ const AdminDashboard = () => {
   )
 }
 
-export default AdminDashboard
+export default AdminDashboard */
+
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './AdminDashboard.css'; // Updated CSS file
+import { assets } from '../../assets/assets';
+import { NavLink } from 'react-router-dom';
+
+// Navbar Component
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img className='logo' src={assets.mylogo} alt="" />
+      <h2>Admin Panel</h2>
+      <img className='profile' src={assets.nejat} alt="" />
+    </div>
+  );
+}
+
+// Sidebar Component
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className='sidebar-options'>
+        <NavLink to='/add' className='sidebar-option'>
+          <img src={assets.add_icon} alt="" />
+          <p>Add Item</p>
+        </NavLink>
+
+        <NavLink to='/list' className='sidebar-option'>
+          <img src={assets.order_icon} alt="" />
+          <p>List Items</p>
+        </NavLink>
+
+        <NavLink to='/orders' className='sidebar-option'>
+          <img src={assets.order_icon} alt="" />
+          <p>Orders</p>
+        </NavLink>
+
+        <NavLink to='/users' className='sidebar-option'>
+          <img src={assets.profile_icon} alt="" />
+          <p>Users</p>
+        </NavLink>
+      </div>
+    </div>
+  );
+}
+
+// AdminDashboard Component combining Navbar and Sidebar
+const AdminDashboard = () => {
+  return (
+    <div className='admin-dashboard'>
+      <Navbar />
+      <Sidebar />
+      {/* You can add more content here if needed */}
+    </div>
+  );
+}
+
+export default AdminDashboard;
+
