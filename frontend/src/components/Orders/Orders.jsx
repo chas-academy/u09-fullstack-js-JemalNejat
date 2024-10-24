511f448c734079d3f744693070b4759315d728f4
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-24 13:19:13 +0200

    Update Orders.jsx

--- frontend/src/components/Orders/Orders.jsx
+++ frontend/src/components/Orders/Orders.jsx
@@ -5,7 +5,8 @@ import React, { useEffect, useState } from 'react'
 import './Orders.css'
 import {toast} from "react-toastify"
 import axios from "axios"
-import { assets } from '../../assets/assets'
+import { assets } from '../../assets/admin_assets/assets'
+
 
 
 const Order = ({url}) => {
