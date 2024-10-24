b5296040d4fec0901ffbf5bc32bb8f57143f666a
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-24 11:40:04 +0200

    Update Users.jsx

--- admin/src/pages/Users/Users.jsx
+++ admin/src/pages/Users/Users.jsx
@@ -3,7 +3,7 @@ import './Users.css';
 import { toast } from 'react-toastify';
 import axios from 'axios';
 
-import { StoreContext } from '@frontend/src/context/StoreContext';
+import { StoreContext } from '@frontend/context/StoreContext.jsx';
 
 
 
