cc336530fc1c8d3d65b8dd383b6ae7da1d84696c
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-24 17:11:28 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,7 @@ import Users from './components/Users/Users';
 
 const App = () => {
   const [showLogin,setShowLogin] = useState(false)
+   const url = "https://u09-fullstack-js-jemalnejat-backend.onrender.com";
 
   return (
     <>
