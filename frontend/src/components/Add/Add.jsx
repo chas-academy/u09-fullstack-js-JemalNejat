bbc00b9aa38a8a327e0c5a4910f35bff2ca67632
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-25 13:54:11 +0200

    Update Add.jsx

--- frontend/src/components/Add/Add.jsx
+++ frontend/src/components/Add/Add.jsx
@@ -6,6 +6,7 @@ import { assets } from '../../assets/admin_assets/assets';
 import axios from "axios"
 import { toast } from 'react-toastify'
 import { Sidebar } from '../../pages/Admin/AdminDashboard';  
+import { Navbar } from '../../pages/Admin/AdminDashboard'; 
  
 
 const Add = ({url}) => {
