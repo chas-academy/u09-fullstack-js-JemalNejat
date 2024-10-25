db63b5d922ad13dc50f8a286b5e119be9019d94c
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-25 13:12:33 +0200

    Update admin.js

--- backend/middleware/admin.js
+++ backend/middleware/admin.js
@@ -1,11 +1,14 @@
 
 
-   const adminMiddleware = (req, res, next) => {
-     if (req.body.userRole !== "admin") {
-       return res.status(403).send("Access denied: Admins only.");
-     }
-     next();
-   };
+   export default function (req, res, next) {
+  if (!req.body.userRole === "admin") {
+    return res.status(403).send("Access denied.");
+  }
+  next();
+}
+
 
    export default adminMiddleware;
+
+
 
