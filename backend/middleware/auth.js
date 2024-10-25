dba73e15081ae2b51ee20f418e200468529f5dcd
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-25 13:12:59 +0200

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,26 +1,24 @@
 import jwt from "jsonwebtoken";
 
 const authMiddleware = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  // Check if authorization header is present and in the correct format
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).send("Not authenticated, please log in.");
+  const { token } = req.headers;
+  if (!token) {
+    return res.status(401).send("Not authenticated, login again!");
   }
-
   try {
-    // Extract token from "Bearer <token>" format
-    const token = authHeader.split(" ")[1];
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Attach user information to the request body
-    req.body.userId = decodedToken.id;
-    req.body.userRole = decodedToken.role;
-    next(); // Proceed to the next middleware or route handler
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.body.userId = token_decode.id;
+    req.body.userRole = token_decode.role;
+    next();
   } catch (error) {
-    console.error("Token verification error:", error);
-    res.status(401).send("Invalid or expired token.");
+    console.log(error);
+    res.status(500).send("Error");
   }
 };
 
 export default authMiddleware;
+
+
+
+
+
