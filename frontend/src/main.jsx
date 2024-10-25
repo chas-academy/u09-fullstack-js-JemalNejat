7d3ca380c7d9cd837574eb76f5ddcff1262c230d
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-25 23:22:15 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
-import { UserProvider } from './context/UserContext.jsx';
+import  UserProvider  from './context/UserContext.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
