ebcdacb397835d46c19239c910c5d43b719b02f3
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-10-26 22:28:15 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,18 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import './index.css'
-// eslint-disable-next-line no-unused-vars
-import { BrowserRouter } from 'react-router-dom'
-import StoreContextProvider from './context/StoreContext.jsx'
+import { BrowserRouter } from "react-router-dom";
+import StoreContextProvider from "./context/StoreContext.jsx";
+import UserProvider from "./context/UserContext.jsx";
 
-import  UserProvider  from './context/UserContext.jsx';
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        <UserProvider>
+            <StoreContextProvider>
+                <App />
+            </StoreContextProvider>
+        </UserProvider>
+    </BrowserRouter>
+);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-   <StoreContextProvider>
-   <UserProvider>
-   <App />
-   </UserProvider>
-   </StoreContextProvider>
-  </BrowserRouter>
-   
-  
-)
