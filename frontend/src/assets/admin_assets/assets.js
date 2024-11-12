d9d9f8b85fd0d0c575f0e2de619e46096df36f1a
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2024-11-12 10:01:35 +0100

    Update assets.js

--- frontend/src/assets/admin_assets/assets.js
+++ frontend/src/assets/admin_assets/assets.js
@@ -1,4 +1,3 @@
-import logo from './logo.png'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
@@ -12,7 +11,6 @@ export const url = "https://u09-fullstack-js-jemalnejat-backend.onrender.com";
   
 
 export const assets ={
-    logo,
     add_icon,
     order_icon,
     profile_image,
