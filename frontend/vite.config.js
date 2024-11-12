6ac183acb601a82164e02a1666df49c0aed4c4fa
Your Name <najat_sf@hotmail.com>
2024-11-12 18:02:42 +0100

    fix PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+import manifest from './public/manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: manifest,
+      workbox: {
+        globPatterns: ['**/*.{js,jsx,css,html,ico,png,jpg}']
+      }
+    }),
+  ],
 })
